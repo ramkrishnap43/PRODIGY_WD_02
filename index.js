e250c4753024a318b998f9c5eb682e67805faca2
ramkrishnap43 <ramkrishnap.prabhu43@gmail.com>
2023-10-12 15:43:20 +0530

    final

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ const lapButton = document.getElementsByClassName("lap")[0];
 const minute = document.getElementsByClassName("minute")[0];
 const second = document.getElementsByClassName("sec")[0];
 const milliSecond = document.getElementsByClassName("msec")[0];
+const laps = document.getElementsByClassName("laps")[0];
+const clearButton = document.getElementsByClassName("clear-button")[0];
+const bg = document.getElementsByClassName("outer")[0];
 
 let isPlay = false;
 let sec;
@@ -12,6 +15,8 @@ let centisec;
 let centiCounter = 0;
 let min;
 let minCounter = 0;
+let isReset = false;
+let lapItem = 0;
 
 const toggleButton = () => {
   lapButton.classList.remove("hidden");
@@ -19,46 +24,81 @@ const toggleButton = () => {
 };
 
 const play = () => {
-  if (!isPlay) {
+  if (!isPlay && !isReset) {
     playButton.innerHTML = "Pause";
+    bg.classList.add("background-animation");
     min = setInterval(() => {
         if(minCounter === 60){
             minCounter = 0
         }
-      minute.innerText = `${++minCounter} : `;
+      minute.innerHTML = `${++minCounter} : `;
     }, 60*1000);
 
     sec = setInterval(() => {
         if(secCounter === 60){
             secCounter = 0
         }
-      second.innerText = `${++secCounter} : `;
+      second.innerHTML = `&nbsp;${++secCounter} : `;
     }, 1000);
     centisec = setInterval(() => {
         if(centiCounter === 100){
             centiCounter = 0
         }
-        milliSecond.innerText = `${++centiCounter}`;
+        milliSecond.innerHTML = `&nbsp;${++centiCounter}`;
       }, 10);
     isPlay = true;
+    isReset = true;
   } else {
     playButton.innerHTML = "Play";
     clearInterval(min)
     clearInterval(sec)
     clearInterval(centisec)
     isPlay = false;
+    isReset = false;
+    bg.classList.remove("background-animation")
   }
   toggleButton();
 };
 
 const reset = () => {
+  isReset = true;
   play();
   lapButton.classList.add("hidden");
   resetButton.classList.add("hidden");
   minute.innerHTML = "0 :"
-  second.innerHTML = "0 :"
-  milliSecond.innerHTML = "0 "
+  second.innerHTML = "&nbsp;0 :"
+  milliSecond.innerHTML = "&nbsp;0 "
 };
+
+const lap = () => {
+
+  const li = document.createElement("li");
+  const number = document.createElement("span");
+  const timeLap = document.createElement("span");
+
+
+  li.setAttribute("class" , "lap-item");
+  number.setAttribute("class" , "number");
+  timeLap.setAttribute("class", "time-lap");
+
+    number.innerText = `${++lapItem}`
+    timeLap.innerHTML = `${minCounter} : ${secCounter} : ${centiCounter}`
+
+    li.append(number , timeLap);
+    laps.append(li)
+
+    clearButton.classList.remove("hidden")
+
+}
+
+const clearAll = () => {
+  laps.innerHTML = " ";
+  laps.append(clearButton);
+  clearButton.classList.add("hidden");
+  lapItem = 0;
+}
 
 playButton.addEventListener("click", play);
 resetButton.addEventListener("click", reset);
+lapButton.addEventListener("click", lap);
+clearButton.addEventListener("click", clearAll)
